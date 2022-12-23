4727bf7d61e96ddd2a0f9bec27318ee075c7ebb4
Sergey Petrenko <Stevo98@mail.ru>
2022-12-23 19:06:38 +1000

    last commit

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -3,8 +3,55 @@ package ru.netology.radio;
 
 public class Radio {
 
-    public byte currentStation;
-    public byte currentVolume;
+    private byte stationsAmount = 10;
+    private byte currentStation;
+    private byte minVolume = 0;
+    private byte maxVolume = 100;
+    private byte currentVolume = minVolume;
+
+    public Radio() {
+        System.out.println(stationsAmount);
+    }
+
+    public Radio(byte Amount) {
+
+    }
+
+    public byte next() {
+        int plus = currentStation +=1;
+        if (currentStation == 10) {
+            currentStation = 0;
+        }
+        setCurrentStation((byte) plus);
+        return currentStation;
+    }
+
+    public byte prev() {
+        int minus = currentStation -=1;
+        if (currentStation == -1) {
+            currentStation = 9;
+        }
+        setCurrentStation((byte) minus);
+        return currentStation;
+    }
+
+    public byte increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = (byte) (currentVolume + 1);
+        }
+        return currentVolume;
+    }
+
+    public byte reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = (byte) (currentVolume - 1);
+        }
+        return currentVolume;
+    }
+
+    public byte getStationsAmount() {
+        return stationsAmount;
+    }
 
     public byte getCurrentStation() {
         return currentStation;
@@ -25,49 +72,16 @@ public class Radio {
 
     }
 
-    public byte next() {
-        if (currentStation <= 9) {
-            currentStation = (byte) (currentStation + 1);
-        }
-        if (currentStation == 10) {
-            currentStation = 0;
-        }
-        return currentStation;
-    }
-
-    public byte prev() {
-        if (currentStation >= 0) {
-            currentStation = (byte) (currentStation - 1);
-        }
-        if (currentStation == -1) {
-            currentStation = 9;
-        }
-        return currentStation;
-    }
-
     public void setCurrentVolume(byte newCurrentVolume) {
         if (newCurrentVolume < 0) {
             return;
         }
-        if (newCurrentVolume > 100) {
+        if (newCurrentVolume > 10) {
             return;
         }
         currentVolume = newCurrentVolume;
     }
-
-    public byte increaseVolume() {
-        if (currentVolume < 100) {
-            currentVolume = (byte) (currentVolume + 1);
-        }
-        return currentVolume;
-    }
-
-    public byte reduceVolume() {
-        if (currentVolume > 0) {
-            currentVolume = (byte) (currentVolume - 1);
-        }
-        return currentVolume;
-    }
 }
+
 
 
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -2,9 +2,20 @@ package ru.netology.radio;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.netology.radio.Radio;
 
 public class RadioTest {
+
+    @Test
+    public void stationsAmountTest() {
+        Radio radio = new Radio((byte) 10);
+        Assertions.assertEquals(10, radio.getStationsAmount());
+    }
+
+    @Test
+    public void stationsAmountTest2() {
+        Radio radio = new Radio();
+        Assertions.assertEquals(10, radio.getStationsAmount());
+    }
 
     @Test
     public void shouldSetStation() {
@@ -183,7 +194,7 @@ public class RadioTest {
     @Test
     public void shouldNotSetVolumeAboveMaxVolume() {
         Radio radio = new Radio();
-        radio.setCurrentVolume((byte) 11);
+        radio.setCurrentVolume((byte) 101);
 
         int expected = 0;
         int actual = radio.getCurrentVolume();
